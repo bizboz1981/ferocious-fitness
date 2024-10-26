4e789b74ebdfca931487cdfa7ffa4fe7417829fa
William Saunders <willjsaunders@icloud.com>
2024-10-26 07:44:25 +0000

    feat: Register Profile model with ProfileAdmin options

--- ferociousfitness/users/admin.py
+++ ferociousfitness/users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Profile
+
+
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = ["user", "subscription_status", "join_date"]
+
+    # Fields to filter by in the admin list view
+    list_filter = ["subscription_status", "join_date"]
+
+    # Fields to search by in the admin search bar
+    search_fields = ["user__username", "user__email"]
+
+
+# Register the Profile model with the ProfileAdmin options
+admin.site.register(Profile, ProfileAdmin)
