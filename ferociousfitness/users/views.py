adf184b19c9fbd269d4c6f6007fda153f411e0ff
William Saunders <willjsaunders@icloud.com>
2024-10-30 16:49:21 +0000

    feat: add form elements to profile.html to manage profiles

--- ferociousfitness/users/views.py
+++ ferociousfitness/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+from .forms import ProfileForm
 from .models import Profile
 
 
@@ -8,7 +9,16 @@ from .models import Profile
 def profile_view(request):
     user = request.user
     profile, created = Profile.objects.get_or_create(user=user)
-    return render(request, "users/profile.html", {"profile": profile})
+
+    if request.method == "POST":
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")
+    else:
+        form = ProfileForm(instance=profile)
+
+    return render(request, "users/profile.html", {"profile": profile, "form": form})
 
 
 def index(request):
