6075bc30b61d7a83b25518cc2efc5d0d3cb7bdb0
William Saunders <willjsaunders@icloud.com>
2024-10-30 16:33:29 +0000

    feat: serve media files during dev

--- ferociousfitness/ferociousfitness/urls.py
+++ ferociousfitness/ferociousfitness/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -35,3 +36,6 @@ if settings.DEBUG:
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
     ]
+
+    # Serve media files during development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
