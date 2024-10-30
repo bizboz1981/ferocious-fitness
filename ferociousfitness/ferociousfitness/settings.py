245f1a2871d8345f7b509deeba18c0d7b7ab778d
William Saunders <willjsaunders@icloud.com>
2024-10-30 16:35:06 +0000

    feat: serve media files dring develpment

--- ferociousfitness/ferociousfitness/settings.py
+++ ferociousfitness/ferociousfitness/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -167,3 +168,6 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
