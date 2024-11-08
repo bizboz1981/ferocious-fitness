6bd551528c51315371bd5f1472ba685c3d2dfe4c
William Saunders <willjsaunders@icloud.com>
2024-11-08 16:28:54 +0000

    feat: register Session and ClassType models in admin panel

--- ferociousfitness/booking/admin.py
+++ ferociousfitness/booking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
+from .models import ClassType, Session
+
+
 # Register your models here.
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "session_type",
+        "date",
+        "time",
+        "location",
+        "max_participants",
+        "current_participants",
+    ]
+
+
+@admin.register(ClassType)
+class ClassTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
