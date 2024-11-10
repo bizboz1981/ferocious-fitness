1ffc02d57af73adac7c607d9fbe932e2dbea9da3
William Saunders <willjsaunders@icloud.com>
2024-11-10 08:02:08 +0000

    feat: add URL pattern for cancel booking functionality

--- ferociousfitness/users/urls.py
+++ ferociousfitness/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from .views import profile_view
+from .views import cancel_booking, profile_view
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("index.html", views.index, name="index"),
     path("profile/", profile_view, name="profile"),
+    path("cancel_booking/<int:booking_id>/", cancel_booking, name="cancel_booking"),
 ]
