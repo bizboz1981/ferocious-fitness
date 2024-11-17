38ab965f20e765895c9466aa6682b1e61659f760
William Saunders <willjsaunders@icloud.com>
2024-11-17 15:47:32 +0000

    feat: add product list and detail views to display products

--- ferociousfitness/products/views.py
+++ ferociousfitness/products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Product
+
 
 # Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, "products/product_list.html", {"products": products})
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, "products/product_detail.html", {"product": product})
