71eca427ea9227542a02edede498b73889b9df33
William Saunders <willjsaunders@icloud.com>
2024-12-08 16:09:58 +0000

    fix: refactor filepaths

--- ferociousfitness/ferociousfitness/wsgi.py
+++ ferociousfitness/ferociousfitness/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ferociousfitness.settings')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "ferociousfitness.ferociousfitness.settings"
+)
 
 application = get_wsgi_application()
