b224abe93da5ceadea53c684e860340d6cbb5ffb
William Saunders <willjsaunders@icloud.com>
2024-12-08 16:13:17 +0000

    refactor dir structure

--- ferociousfitness/ferociousfitness/wsgi.py
+++ ferociousfitness/ferociousfitness/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "ferociousfitness.ferociousfitness.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
