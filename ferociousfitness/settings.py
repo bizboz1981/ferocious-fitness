6d5bb612d8eb8fdce86c0c93a2bd88fab37a1d1f
William Saunders <willjsaunders@icloud.com>
2024-12-12 12:08:10 +0000

    refactor: remove S3 code, add url patterns

--- ferociousfitness/settings.py
+++ ferociousfitness/settings.py
@@ -168,6 +168,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -182,20 +183,20 @@ INTERNAL_IPS = [
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-if "USE_AWS" in os.environ:
-    AWS_STORAGE_BUCKET_NAME = "ferociousfitness"
-    AWS_S3_REGION_NAME = "eu-north-1"
-    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+# if "USE_AWS" in os.environ:
+#     AWS_STORAGE_BUCKET_NAME = "ferociousfitness"
+#     AWS_S3_REGION_NAME = "eu-north-1"
+#     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+#     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-    STATICFILES_STORAGE = "custom_storages.StaticStorage"
-    STATICFILES_LOCATION = "static"
-    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
-    MEDIAFILES_LOCATION = "media"
+#     STATICFILES_STORAGE = "custom_storages.StaticStorage"
+#     STATICFILES_LOCATION = "static"
+#     DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+#     MEDIAFILES_LOCATION = "media"
 
-    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 MESSAGE_TAGS = {
     messages.DEBUG: "debug",
