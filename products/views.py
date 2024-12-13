2bb88df7e2feda756ac25db45978f8a7df472c9e
William Saunders <willjsaunders@icloud.com>
2024-12-13 10:23:34 +0000

    feat: add search and sorting functionality to product list

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, render
 
 from .models import Product
@@ -6,10 +7,24 @@ from .models import Product
 # Create your views here.
 def product_list(request):
     category_name = request.GET.get("category")
+    search_query = request.GET.get("search")
+    sort_by = request.GET.get("sort_by")
+
+    products = Product.objects.all()
+
     if category_name:
-        products = Product.objects.filter(category__name=category_name)
-    else:
-        products = Product.objects.all()
+        products = products.filter(category__name=category_name)
+
+    if search_query:
+        products = products.filter(
+            Q(name__icontains=search_query) | Q(description__icontains=search_query)
+        )
+
+    if sort_by == "price_asc":
+        products = products.order_by("price")
+    elif sort_by == "price_desc":
+        products = products.order_by("-price")
+
     return render(request, "products/product_list.html", {"products": products})
 
 
